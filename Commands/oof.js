23be3daefc58daed36936d0763bff41f77dd5e6d
shinyo1357 <shinyo1357@gmail.com>
2023-01-07 13:34:10 +0800

    update oof

--- Commands/oof.js
+++ Commands/oof.js
@@ -4,24 +4,23 @@ const oof =  {
     name: keyword,
     run: async (message) => {
             const channel = message.member.voice.channel
-            setTimeout(() =>{
             if(channel !== null){
                 const connection = joinVoiceChannel({
                     channelId: channel.id,
                     guildId: channel.guild.id,
                     adapterCreator: channel.guild.voiceAdapterCreator,
                 });
+                setTimeout(() =>{
                 const player = createAudioPlayer();
                 const resource = createAudioResource('C:/Users/shinyo/Documents/GitHub/LEO/voice/oof.mp3');
                 connection.subscribe(player);
                 player.play(resource);
                 message.channel.send("正在播放音效"+`***${keyword}***`)
+                }, 500)
             }
             else{
                 message.channel.send('你需要先加入語音頻道');
-            }}
-            ,500)
-            
+            }
             
         }
     
